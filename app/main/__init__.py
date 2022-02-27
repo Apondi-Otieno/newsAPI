64c4185166ad7fc3bc4365914736d99422f3c538
Apondi Otieno <apondotii@gmail.com>
2022-02-27 11:35:11 +0300

    this will connect our app to the configuration file

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -3,6 +3,10 @@ from flask import Flask
 # Initializing application
 app = Flask(__name__)
 
+# Setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
 from app import views
 
 
