20d499bb76490fcc3d99502db1925344c0f0ca4a
Apondi Otieno <apondotii@gmail.com>
2022-02-27 11:32:38 +0300

    this will add three classes to the config file

--- virtual/config.py
+++ virtual/config.py
@@ -1,1 +1,29 @@
 NEWS_API_KEY='< 9c6c93421cdc4624835c17adb457d9e7>'
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    pass
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
