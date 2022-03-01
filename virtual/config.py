9380e9a0e984f64439b9213f8e9a213d33f9a83f
Apondi-Otieno <apondiotii@gmail.com>
2022-03-01 15:22:05 +0300

    deployment to heroku

--- virtual/config.py
+++ virtual/config.py
@@ -8,9 +8,9 @@ class Config:
 
 class Config:
     '''
-    General configuration parent class
+    General configuration of the parent class
     '''
-  
+ 
     NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?country=us&apiKey=9c6c93421cdc4624835c17adb457d9e7'
     NEWS_SOURCE_BASE_URL='https://newsapi.org/v2/top-headlines/sources?apiKey=9c6c93421cdc4624835c17adb457d9e7'
     NEWS_API_KEY =os.environ.get('9c6c93421cdc4624835c17adb457d9e7')
@@ -18,7 +18,7 @@ class Config:
 
 class ProdConfig(Config):
     '''
-    Production  configuration child class
+    Production  configuration of the child class
 
     Args:
         Config: The parent configuration class with General configuration settings
